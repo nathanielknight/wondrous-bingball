370ceca7134cf5bf0e8d3db5d3f164a9a0f642f3
Nat Knight <nathaniel.ep@gmail.com>
2018-05-02 19:23:26 -0700

    Move ball outside paddle when reflecting
    Otherwise the ball can pass through the paddle for several frames,
    gaining velocity and "bouncing" forward with each frame.

--- src/game.rs
+++ src/game.rs
@@ -67,11 +67,17 @@ impl Ball {
         let theta = std::f32::consts::PI / 4.0 * dy / r.h;
         let speed =
             (self.velocity.x * self.velocity.x + self.velocity.y * self.velocity.y).sqrt() * 1.1;
-        // set velocity direction
+        // Set velocity direction.
         self.velocity.x = -1.0 * self.velocity.x.signum() * theta.cos();
         self.velocity.y = theta.sin();
-        // set velocity magnitude
+        // Set velocity magnitude.
         self.velocity *= speed;
+        // Set position to outside of bounce paddle.
+        if self.velocity.x.signum() > 0.0 {
+            self.rect.x = r.right();
+        } else {
+            self.rect.x = r.left();
+        }
     }
 
     fn draw(&self, ctx: &mut ggez::Context) -> ggez::GameResult<()> {
